120ce030b1d69a60c9a675cba98acee06e02da3b
Marcus Reinhardt <marcus.reinhardt@deutschebahn.com>
2025-07-19 15:16:09 +0200

    disable collections and link-check to test the pipeline

--- apps/docs/src/collections.ts
+++ apps/docs/src/collections.ts
@@ -1,133 +1,133 @@
-import type { z } from "zod"
-import { EntryGroup, isDirectory, isFile } from "renoun/file-system"
+// import type { z } from "zod"
+// import { EntryGroup, isDirectory, isFile } from "renoun/file-system"
 
-import type { frontmatterSchema } from "./validations"
-import { removeFromArray } from "./lib/utils"
-import { generateDirectories } from "./sources"
+// import type { frontmatterSchema } from "./validations"
+// import { removeFromArray } from "./lib/utils"
+// import { generateDirectories } from "./sources"
 
-export const DocumentationGroup = new EntryGroup({
-  entries: [...generateDirectories()],
-})
+// export const DocumentationGroup = new EntryGroup({
+//   entries: [...generateDirectories()],
+// })
 
-export type EntryType = Awaited<ReturnType<typeof DocumentationGroup.getEntry>>
-export type DirectoryType = Awaited<ReturnType<typeof DocumentationGroup.getDirectory>>
+// export type EntryType = Awaited<ReturnType<typeof DocumentationGroup.getEntry>>
+// export type DirectoryType = Awaited<ReturnType<typeof DocumentationGroup.getDirectory>>
 
-/**
- * Helper function to get the title for an element in the sidebar/navigation
- * @param collection {EntryType} the collection to get the title for
- * @param frontmatter {z.infer<typeof frontmatterSchema>} the frontmatter to get the title from
- * @param includeTitle? {boolean} whether to include the title in the returned string
- * @returns {string} the title to be displayed in the sidebar/navigation
- */
-export function getTitle(
-  collection: EntryType,
-  frontmatter: z.infer<typeof frontmatterSchema>,
-  includeTitle = false,
-): string {
-  return includeTitle
-    ? (frontmatter.navTitle ?? frontmatter.title ?? collection.getTitle())
-    : (frontmatter.navTitle ?? collection.getTitle())
-}
+// /**
+//  * Helper function to get the title for an element in the sidebar/navigation
+//  * @param collection {EntryType} the collection to get the title for
+//  * @param frontmatter {z.infer<typeof frontmatterSchema>} the frontmatter to get the title from
+//  * @param includeTitle? {boolean} whether to include the title in the returned string
+//  * @returns {string} the title to be displayed in the sidebar/navigation
+//  */
+// export function getTitle(
+//   collection: EntryType,
+//   frontmatter: z.infer<typeof frontmatterSchema>,
+//   includeTitle = false,
+// ): string {
+//   return includeTitle
+//     ? (frontmatter.navTitle ?? frontmatter.title ?? collection.getTitle())
+//     : (frontmatter.navTitle ?? collection.getTitle())
+// }
 
-/**
- * Helper function to get the file content for a given source entry
- * This function will try to get the file based on the given path and the "mdx" extension
- * If the file is not found, it will try to get the index file based on the given path and the "mdx" extension
- * If there is also no index file, it will return null
- *
- * @param source {EntryType} the source entry to get the file content for
- */
-export const getFileContent = async (source: EntryType) => {
-  // first, try to get the file based on the given path
+// /**
+//  * Helper function to get the file content for a given source entry
+//  * This function will try to get the file based on the given path and the "mdx" extension
+//  * If the file is not found, it will try to get the index file based on the given path and the "mdx" extension
+//  * If there is also no index file, it will return null
+//  *
+//  * @param source {EntryType} the source entry to get the file content for
+//  */
+// export const getFileContent = async (source: EntryType) => {
+//   // first, try to get the file based on the given path
 
-  return await DocumentationGroup.getFile(source.getPathSegments(), "mdx").catch(async () => {
-    return await DocumentationGroup.getFile([...source.getPathSegments(), "index"], "mdx").catch(
-      () => null,
-    )
-  })
-}
+//   return await DocumentationGroup.getFile(source.getPathSegments(), "mdx").catch(async () => {
+//     return await DocumentationGroup.getFile([...source.getPathSegments(), "index"], "mdx").catch(
+//       () => null,
+//     )
+//   })
+// }
 
-/**
- * Helper function to get the sections for a given source entry
- * This function will try to get the sections based on the given path
- *
- * If there there are no entries/children for the current path, it will return an empty array
- *
- * @param source {EntryType} the source entry to get the sections for
- * @returns
- */
-export async function getSections(source: EntryType) {
-  if (source.getDepth() > -1) {
-    if (isDirectory(source)) {
-      return (
-        await (await DocumentationGroup.getDirectory(source.getPathSegments())).getEntries()
-      ).filter((ele) => ele.getPath() !== source.getPath())
-    }
+// /**
+//  * Helper function to get the sections for a given source entry
+//  * This function will try to get the sections based on the given path
+//  *
+//  * If there there are no entries/children for the current path, it will return an empty array
+//  *
+//  * @param source {EntryType} the source entry to get the sections for
+//  * @returns
+//  */
+// export async function getSections(source: EntryType) {
+//   if (source.getDepth() > -1) {
+//     if (isDirectory(source)) {
+//       return (
+//         await (await DocumentationGroup.getDirectory(source.getPathSegments())).getEntries()
+//       ).filter((ele) => ele.getPath() !== source.getPath())
+//     }
 
-    if (isFile(source) && source.getBaseName() === "index") {
-      return await source.getParent().getEntries()
-    }
-    return []
-  } else {
-    return (
-      await (await DocumentationGroup.getDirectory(source.getPathSegments())).getEntries()
-    ).filter((ele) => ele.getPath() !== source.getPath())
-  }
-}
+//     if (isFile(source) && source.getBaseName() === "index") {
+//       return await source.getParent().getEntries()
+//     }
+//     return []
+//   } else {
+//     return (
+//       await (await DocumentationGroup.getDirectory(source.getPathSegments())).getEntries()
+//     ).filter((ele) => ele.getPath() !== source.getPath())
+//   }
+// }
 
-/**
- * Helper function to get the breadcrumb items for a given slug
- *
- * @param slug {string[]} the slug to get the breadcrumb items for
- */
-export const getBreadcrumbItems = async (slug: string[]) => {
-  // we do not want to have "index" as breadcrumb element
-  const cleanedSlug = removeFromArray(slug, ["index"])
+// /**
+//  * Helper function to get the breadcrumb items for a given slug
+//  *
+//  * @param slug {string[]} the slug to get the breadcrumb items for
+//  */
+// export const getBreadcrumbItems = async (slug: string[]) => {
+//   // we do not want to have "index" as breadcrumb element
+//   const cleanedSlug = removeFromArray(slug, ["index"])
 
-  const combinations = cleanedSlug.map((_, index) => cleanedSlug.slice(0, index + 1))
+//   const combinations = cleanedSlug.map((_, index) => cleanedSlug.slice(0, index + 1))
 
-  const items = []
+//   const items = []
 
-  for (const currentPageSegement of combinations) {
-    let collection: EntryType
-    let file: Awaited<ReturnType<typeof getFileContent>>
-    let frontmatter: z.infer<typeof frontmatterSchema> | undefined
-    try {
-      collection = await DocumentationGroup.getEntry(currentPageSegement)
-      if (collection.getPathSegments().includes("index")) {
-        file = await getFileContent(collection.getParent())
-      } else {
-        file = await getFileContent(collection)
-      }
+//   for (const currentPageSegement of combinations) {
+//     let collection: EntryType
+//     let file: Awaited<ReturnType<typeof getFileContent>>
+//     let frontmatter: z.infer<typeof frontmatterSchema> | undefined
+//     try {
+//       collection = await DocumentationGroup.getEntry(currentPageSegement)
+//       if (collection.getPathSegments().includes("index")) {
+//         file = await getFileContent(collection.getParent())
+//       } else {
+//         file = await getFileContent(collection)
+//       }
 
-      frontmatter = await file?.getExportValue("frontmatter")
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    } catch (e: unknown) {
-      continue
-    }
+//       frontmatter = await file?.getExportValue("frontmatter")
+//       // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//     } catch (e: unknown) {
+//       continue
+//     }
 
-    if (!frontmatter) {
-      items.push({
-        title: collection.getTitle(),
-        path: ["docs", ...collection.getPathSegments()],
-      })
-    } else {
-      const title = getTitle(collection, frontmatter, true)
-      items.push({
-        title,
-        path: ["docs", ...removeFromArray(collection.getPathSegments(), ["index"])],
-      })
-    }
-  }
+//     if (!frontmatter) {
+//       items.push({
+//         title: collection.getTitle(),
+//         path: ["docs", ...collection.getPathSegments()],
+//       })
+//     } else {
+//       const title = getTitle(collection, frontmatter, true)
+//       items.push({
+//         title,
+//         path: ["docs", ...removeFromArray(collection.getPathSegments(), ["index"])],
+//       })
+//     }
+//   }
 
-  return items
-}
+//   return items
+// }
 
-/**
- * Checks if an entry is hidden (starts with an underscore)
- *
- * @param entry {EntryType} the entry to check for visibility
- */
-export function isHidden(entry: EntryType) {
-  return entry.getBaseName().startsWith("_")
-}
+// /**
+//  * Checks if an entry is hidden (starts with an underscore)
+//  *
+//  * @param entry {EntryType} the entry to check for visibility
+//  */
+// export function isHidden(entry: EntryType) {
+//   return entry.getBaseName().startsWith("_")
+// }
--- apps/docs/src/link-check.ts
+++ apps/docs/src/link-check.ts
@@ -1,121 +1,121 @@
-import { readFileSync } from "fs"
-import path from "path"
-import { toString } from "mdast-util-to-string"
-import { printErrors, scanURLs, validateFiles } from "next-validate-link"
-import remarkParse from "remark-parse"
-import { isDirectory, isFile } from "renoun/file-system"
-import { unified } from "unified"
+// import { readFileSync } from "fs"
+// import path from "path"
+// import { toString } from "mdast-util-to-string"
+// import { printErrors, scanURLs, validateFiles } from "next-validate-link"
+// import remarkParse from "remark-parse"
+// import { isDirectory, isFile } from "renoun/file-system"
+// import { unified } from "unified"
 
-import { DocumentationGroup } from "./collections"
-import { createSlug, removeFromArray } from "./lib/utils"
+// import { DocumentationGroup } from "./collections"
+// import { createSlug, removeFromArray } from "./lib/utils"
 
-const getDirectories = async () => {
-  const entries = (await DocumentationGroup.getEntries({ recursive: true })).filter((entry) =>
-    isDirectory(entry),
-  )
+// const getDirectories = async () => {
+//   const entries = (await DocumentationGroup.getEntries({ recursive: true })).filter((entry) =>
+//     isDirectory(entry),
+//   )
 
-  const result = []
-  for (const entry of entries) {
-    const filePath = entry.getAbsolutePath()
+//   const result = []
+//   for (const entry of entries) {
+//     const filePath = entry.getAbsolutePath()
 
-    const slug = [...entry.getPathSegments()]
-    const url = `/${path.join("docs", ...slug)}`
+//     const slug = [...entry.getPathSegments()]
+//     const url = `/${path.join("docs", ...slug)}`
 
-    result.push({
-      path: filePath,
-      url,
-      slug,
-    })
-  }
+//     result.push({
+//       path: filePath,
+//       url,
+//       slug,
+//     })
+//   }
 
-  return result
-}
+//   return result
+// }
 
-const getFiles = async () => {
-  const entries = (
-    await DocumentationGroup.getEntries({
-      recursive: true,
-      includeIndexAndReadme: true,
-    })
-  ).filter((entry) => isFile(entry, "mdx"))
+// const getFiles = async () => {
+//   const entries = (
+//     await DocumentationGroup.getEntries({
+//       recursive: true,
+//       includeIndexAndReadme: true,
+//     })
+//   ).filter((entry) => isFile(entry, "mdx"))
 
-  const result = []
-  for (const entry of entries) {
-    const filePath = entry.getAbsolutePath()
+//   const result = []
+//   for (const entry of entries) {
+//     const filePath = entry.getAbsolutePath()
 
-    const slug = removeFromArray(entry.getPathSegments(), ["index"])
-    const url = `/${path.join("docs", ...slug)}`
-    const content = readFileSync(filePath).toString()
+//     const slug = removeFromArray(entry.getPathSegments(), ["index"])
+//     const url = `/${path.join("docs", ...slug)}`
+//     const content = readFileSync(filePath).toString()
 
-    const file = unified()
-      .use(remarkParse)
-      // it seems that something is weird with the headings
-      // because the tree isn't updated with the id's
-      //.use({ plugins: remarkPlugins })
-      .parse(content)
+//     const file = unified()
+//       .use(remarkParse)
+//       // it seems that something is weird with the headings
+//       // because the tree isn't updated with the id's
+//       //.use({ plugins: remarkPlugins })
+//       .parse(content)
 
-    const headinsFromTree = file.children.filter((node) => node.type === "heading")
+//     const headinsFromTree = file.children.filter((node) => node.type === "heading")
 
-    // this is just a copy from
-    // https://github.com/souporserious/renoun/blob/main/packages/mdx/src/remark/add-headings.ts
-    const headingCounts = new Map()
-    const headings = headinsFromTree.map((node) => {
-      const text = node.children.map((child) => toString(child)).join("")
-      let id = createSlug(text)
+//     // this is just a copy from
+//     // https://github.com/souporserious/renoun/blob/main/packages/mdx/src/remark/add-headings.ts
+//     const headingCounts = new Map()
+//     const headings = headinsFromTree.map((node) => {
+//       const text = node.children.map((child) => toString(child)).join("")
+//       let id = createSlug(text)
 
-      if (headingCounts.has(id)) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        const count = headingCounts.get(id) + 1
-        headingCounts.set(id, count)
-        id = `${id}-${count}`
-      } else {
-        headingCounts.set(id, 1)
-      }
+//       if (headingCounts.has(id)) {
+//         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+//         const count = headingCounts.get(id) + 1
+//         headingCounts.set(id, count)
+//         id = `${id}-${count}`
+//       } else {
+//         headingCounts.set(id, 1)
+//       }
 
-      return {
-        text,
-        id,
-        depth: node.depth,
-      }
-    })
+//       return {
+//         text,
+//         id,
+//         depth: node.depth,
+//       }
+//     })
 
-    result.push({
-      path: filePath,
-      url,
-      slug,
-      content,
-      headings: headings.map((ele) => ele.id),
-    })
-  }
+//     result.push({
+//       path: filePath,
+//       url,
+//       slug,
+//       content,
+//       headings: headings.map((ele) => ele.id),
+//     })
+//   }
 
-  return result
-}
+//   return result
+// }
 
-const files = await getFiles()
+// const files = await getFiles()
 
-const directories = await getDirectories()
+// const directories = await getDirectories()
 
-const scanned = await scanURLs({
-  pages: ["docs/[...slug]/page.tsx"],
+// const scanned = await scanURLs({
+//   pages: ["docs/[...slug]/page.tsx"],
 
-  populate: {
-    "docs/[...slug]": [
-      ...files.map((collection) => ({
-        value: collection.slug,
-        hashes: collection.headings,
-      })),
-      ...directories.map((collection) => ({
-        value: collection.slug,
-      })),
-    ],
-  },
-})
+//   populate: {
+//     "docs/[...slug]": [
+//       ...files.map((collection) => ({
+//         value: collection.slug,
+//         hashes: collection.headings,
+//       })),
+//       ...directories.map((collection) => ({
+//         value: collection.slug,
+//       })),
+//     ],
+//   },
+// })
 
-// console.dir(scanned)
+// // console.dir(scanned)
 
-const validateLinks = await validateFiles(files, {
-  scanned,
-  checkExternal: false,
-})
+// const validateLinks = await validateFiles(files, {
+//   scanned,
+//   checkExternal: false,
+// })
 
-printErrors(validateLinks, true)
+// printErrors(validateLinks, true)
