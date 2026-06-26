cf328464350cbfe1a47954c9424de01df276020b
Marcus Reinhardt <webstone@gmail.com>
2026-06-26 10:41:34 +0200

    updates

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,11 @@
-import tsconfigPaths from "vite-tsconfig-paths"
 import { defineConfig, Plugin } from "vitest/config"
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
   test: {
     projects: ["packages/*"],
     environment: "node",
     coverage: {
       provider: "v8",
-      all: true,
       include: ["packages/*/src/**/*.{ts,tsx}"],
       exclude: ["**/*.d.ts", "*.config.(mjs|js|ts)", "vitest.config/"],
     },
